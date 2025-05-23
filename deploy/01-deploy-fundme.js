de9709132eafb4f8cb4a1441a633e5d7167cb19a
Balachandar Mutharasu <balachandar.mutharasu@gmail.com>
2025-05-23 23:47:19 +0530

    Deployed  fund me with price feed

--- deploy/01-deploy-fundme.js
+++ deploy/01-deploy-fundme.js
@@ -14,18 +14,29 @@ const { config } = require("process");
 
 //3. get required values from hre
 
-const { networkConfig } = require("../helper-hardhat-config.js");
+const {
+  networkConfig,
+  developmentChains,
+} = require("../helper-hardhat-config.js");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
   const chainId = network.config.chainId;
 
-  const ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  let ethUsdPriceFeedAddress;
+  if (developmentChains.includes(network.name)) {
+    const ethUSDAggregator = await deployments.get("MockV3Aggregator");
+    ethUsdPriceFeedAddress = ethUSDAggregator.address;
+  } else {
+    ethUsdPriceFeedAddress = networkConfig[chainId]["ethUsdPriceFeed"];
+  }
 
   const fundMe = await deploy("FundMe", {
     from: deployer,
     args: [address],
     log: true,
   });
+
+  module.exports.tags = ["all", "fundMe"];
 };
